4ea9ae9ed68107b1dc5d8c8fca9c28a944f89e7c
bharishh <harish_01@icloud.com>
2025-11-04 18:11:17 +0100

    Liste etudiant avec leurs posts

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,23 @@
 import { useState } from 'react'
-import User from "./components/User";
+import User from "./components/User"
 import './App.css'
-
 function App() {
-  const [users, setUsers] = useState ([
+  const [users, setUsers] = useState (() => {
+
+    const saved = localStorage.getItem("users");
+    if (saved) return JSON.parse(saved);
+
+    return [
     {
       id : 1,
       name: "Harish Balakirishnan",
       email: "harish@gmail.com",
-      image : "",
+      image : "https://media.istockphoto.com/id/1171169099/fr/photo/homme-avec-les-bras-crois%C3%A9s-disolement-sur-le-fond-gris.jpg?s=612x612&w=0&k=20&c=csQeB3utGtrGeb3WmdSxRYXaJvUy_xqlhbOIZxclcGA=",
       posts : [
         {
-          type : "message", content : "",
-          type : "photo", content : ""
+          type: "message", content: "Salut tout le monde !",   date: new Date().toLocaleString(),
         }
-      ]
+      ],
     },
     {
       id :2,
@@ -23,14 +26,28 @@ function App() {
       image : "",
       posts : [
         {
-          type : "message", content : "",
-          type : "photo", content : ""
+          typePhoto: "photo", content : "https://i.etsystatic.com/30101587/r/il/c048ef/3801953885/il_fullxfull.3801953885_tqnn.jpg",  date: new Date().toLocaleString(),
         }
       ]
     }
-  ])
 
+  ]})
+  const addPost =  (userId, newPost) => {
+    setUsers(users.map(user =>
+      user.id === userId ? {...user, posts:[ ...user.posts,{id: Date.now(),  ...newPost, date: new Date().toLocaleString(),}]} : user
+    ));
+  }
 
+  return (
+    <> 
+    <div> 
+      <h1>Liste des utilisateurs</h1> 
+    {users.map(user => ( 
+      <User key={user.id} user={user}  onAddPost={addPost} /> ))}
+       </div> 
+    </>
+  )
 }
+
 
 export default App
--- src/components/User.jsx
+++ src/components/User.jsx
@@ -1,19 +1,43 @@
-import PostList from "./PostList";
-import AddPostForm from "./AddPostForm";
 import { useState } from "react";
 
-function User({user, onAddPost})
-{
+ //List Etudiant
 
-    const [postType, setPostType] = useState("message");
-    const [imageFile, setImageFile] = useState(null);
-    const [postContent, setContent] = useState("");
 
+
+function User({ user}) {
+  return (
+
+     <div>
+      <h2>{user.name}</h2>  
+      <img
+        src={user.image}
+        alt={user.name}
+      />
+
+      <h3>Publications :</h3>
+      {user.posts.length === 0 ? (
+        <p>Aucune publication</p>
+      ) : (
+        user.posts.map((post, index) => (
+          <div key={index}>
+            {post.typePhoto === "photo" ? (
+              <img
+                src={post.content}
+                alt="publication"
+                style={{ maxWidth: 200}}
+              />
+            ) : (
+              <p>{post.content}</p>
+            )}
+    
+              <small>
+                Publié le : {post.date}
+              </small>
+          </div>
+        ))
+      )}
+    </div>
+  );
 }
-
-
-
-
-
 
 export default User;
