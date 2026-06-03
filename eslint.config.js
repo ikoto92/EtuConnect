c9e25fe62ff72c4db32c8cd48db75b143856873b
André IKOTO <126103789+ikoto92@users.noreply.github.com>
2026-06-03 10:49:49 +0200

    Update eslint.config.js

--- eslint.config.js
+++ eslint.config.js
@@ -7,12 +7,14 @@ import { defineConfig, globalIgnores } from 'eslint/config'
 export default defineConfig([
   globalIgnores(['dist']),
   {
+    
     files: ['**/*.{js,jsx}'],
     extends: [
       js.configs.recommended,
       reactHooks.configs['recommended-latest'],
       reactRefresh.configs.vite,
     ],
+    
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
